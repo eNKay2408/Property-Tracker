9fe51be70748a18f853f4708f00a4eefb1fb509e
eNKay <nguyenkhai24082408@gmail.com>
2024-09-09 02:47:18 +0700

    chore: add assets and constants
    - Imported logo.svg and initialized index.js in assets for managing media resources
    - Added index.ts in constants for managing project-wide constants

---
+++ client/src/constants/index.ts
@@ -0,0 +1,27 @@
+export const propertyReferralsInfo = [
+	{
+		title: "Social Media",
+		percentage: 64,
+		color: "#F38181",
+	},
+	{
+		title: "Marketplace",
+		percentage: 40,
+		color: "#95E1D3",
+	},
+	{
+		title: "Websites",
+		percentage: 50,
+		color: "#FCE38A",
+	},
+	{
+		title: "Digital Ads",
+		percentage: 80,
+		color: "#3FC1C9",
+	},
+	{
+		title: "Others",
+		percentage: 15,
+		color: "#3F72AF",
+	},
+];
