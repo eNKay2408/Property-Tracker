ce93489d5bf0a6432d88eb6e5a99613d2a601a20
eNKay <nguyenkhai24082408@gmail.com>
2024-09-13 01:00:46 +0700

    chore: export common and charts components for streamlined imports

---
+++ client/src/components/index.ts
@@ -0,0 +1,22 @@
+// common
+import Profile from "./common/Profile";
+import PropertyCard from "./common/PropertyCard";
+import CustomButton from "./common/CustomButton";
+import AgentCard from "./common/AgentCard";
+import Form from "./common/Form";
+
+// charts
+import PieChart from "./charts/PieChart";
+import PropertyReferrals from "./charts/PropertyReferrals";
+import TotalRevenue from "./charts/TotalRevenue";
+
+export {
+	Profile,
+	PropertyCard,
+	CustomButton,
+	PieChart,
+	PropertyReferrals,
+	TotalRevenue,
+	AgentCard,
+	Form,
+};
